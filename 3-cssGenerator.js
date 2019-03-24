70fc549df45b9b3d147d374fb67a6c1e10430e08
Rustan-ISAKOV <45059671+Rustan-ISAKOV@users.noreply.github.com>
2019-03-24 20:01:53 +0600

    Update 3-cssGenerator.js

--- 3-cssGenerator.js
+++ 3-cssGenerator.js
@@ -8,7 +8,6 @@ function fReSizeRange() {
 	fExamChange(toReSizeWidth, toReSizeHeight);	
 }
 
-
 //изменение ширины и высоты по вводимым значениям в поле ввода
 function fInputData(){
 	var toInputWidth = document.getElementById("sWText").value;
