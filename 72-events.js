52d4ba4697aa211896e3100c39a9418b395deeaa
Rustan-ISAKOV <RustanISAKOV@gmail.com>
2019-03-25 20:27:41 +0600

    'Alex'

---
+++ 72-events.js
@@ -0,0 +1,38 @@
+"use strict";
+
+var str = 'Hello, my friend!';
+alert(str.toUpperCase);
+
+//Напишите функцию checkSpam(str), которая возвращает true, если строка str содержит „viagra“ или „XXX“, а иначе false.
+/*var str = 'please buy viagra';
+if (~str.indexOf('viagra')){
+	alert('SPAM!')
+} else alert('No SPAM!');*/
+
+
+/*alert('Oka' [0].toLowerCase())*/
+
+//Напишите функцию ucFirst(str), которая возвращает строку str с заглавным первым символом, например:
+/*var fName = 'vasya';
+fName = fName.charAt(0).toUpperCase() + fName.substring(1);
+alert(fName);*/
+
+/*//ввод числа через строку ввода
+var Num = prompt('Введите число:');
+alert ('Вы ввели число:' + Num);
+var nameds = prompt("Введите число", "-12.5");
+alert(nameds);
+btn.onclick = fOnClick;*/
+
+/*function fOnClick() {
+	// скрывает текст
+	document.getElementById('hideMe').style.display = "none";
+	//скрывает сам элемент по которому кликнули
+	this.style.display = "none";
+}*/
+
+/*
+menu.onclick = fDropDown;
+function fDropDown() {
+	document.getElementById('mainmenu').style.display = "none";
+}*/
