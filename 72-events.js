3a48f9830c26b00798ccca295dddeaa7f610ff41
Rustan-ISAKOV <45059671+Rustan-ISAKOV@users.noreply.github.com>
2019-06-11 00:19:36 +0600

    Update 72-events.js

--- 72-events.js
+++ 72-events.js
@@ -1,4 +1,4 @@
-"use strict";                                  
+"use strict";                                   
                                                                                                                                                         
 var str = 'Hello, my friend!';                              
                                                      
