7cca90ac76303c8c5951af79a6b8371cb25e83f4
Rustan-ISAKOV <45059671+Rustan-ISAKOV@users.noreply.github.com>
2019-06-12 13:31:46 +0600

    Update 72-events.js

--- 72-events.js
+++ 72-events.js
@@ -3,7 +3,7 @@
 var str = 'Hello, my friend!';                              
                                                       
 alert(str.toUpperCase);                   
-           
+            
 //Напишите функцию checkSpam(str), которая возвращает true, если строка str содержит „viagra“ или „XXX“, а иначе false.
 /*var str = 'please buy viagra';
 if (~str.indexOf('viagra')){
