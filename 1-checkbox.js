dbc3a00606f512cecef910c0ab1125f0f1c4ae0f
Rustan-ISAKOV <45059671+Rustan-ISAKOV@users.noreply.github.com>
2020-07-12 14:02:01 +0600

    Update 1-checkbox.js

--- 1-checkbox.js
+++ 1-checkbox.js
@@ -5,8 +5,7 @@
 		document.getElementById("fText").style.display = "inline";    
 	} else document.getElementById("fText").style.display = "none";
 }*/                                                                                                                                                                                                                          
-//в поле вводится инфо и от  ображается в текстовом поле                    
-function onClickMe() {       
+//в поле вводится инфо и от  ображается в текстовом поле              
 	if (document.getElementById("Chk").checked) {     
 		document.getElementById("fText").style.display = "inline";
 		document.getElementById("parag").style.display = "block";
