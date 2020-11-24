991683e6a1fec6fc2339a3cd0517fea1f0ecb060
Rustan-ISAKOV <45059671+Rustan-ISAKOV@users.noreply.github.com>
2020-11-24 20:29:15 +0600

    Update 1-checkbox.js

--- 1-checkbox.js
+++ 1-checkbox.js
@@ -3,7 +3,7 @@
 	var CheckCond = document.getElementById("Chk");                            
 	if (CheckCond.checked) {                 
 		document.getElementById("fText").style.display = "inline";         
-}*/                                                                                                                                                                                                                                
+}*/                                                                                                                                                                                                                                 
 //в поле вводится инфо и от  ображается в текстовом поле                           
 	if (document.getElementById("Chk").checked) {              
 		document.getElementById("fText").style.display = "inline";      
