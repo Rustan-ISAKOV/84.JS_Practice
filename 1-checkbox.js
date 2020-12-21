76b1c98dd7209206630ff73ecde01cfd1ee0f2b2
Rustan-ISAKOV <45059671+Rustan-ISAKOV@users.noreply.github.com>
2020-12-21 22:27:59 +0600

    Update 1-checkbox.js

--- 1-checkbox.js
+++ 1-checkbox.js
@@ -1,7 +1,7 @@
 // on/Off текстового поля по checkbox   
 /*function onClickMe() {                   
 	var CheckCond = document.getElementById("Chk");                              
-	if (CheckCond.checked) {                       
+	if (CheckCond.checked) {                        
 		document.getElementById("fText").style.display = "inline";               
 }*/                                                                                                                                                                                                                                       
 //в поле вводится инфо и от  ображается в текстовом поле                                        
