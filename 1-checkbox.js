09e0c5df54c83e89f6bea2b05a369a4c07e309e3
Rustan-ISAKOV <45059671+Rustan-ISAKOV@users.noreply.github.com>
2020-12-22 17:29:18 +0600

    Update 1-checkbox.js

--- 1-checkbox.js
+++ 1-checkbox.js
@@ -2,7 +2,7 @@
 /*function onClickMe() {                   
 	var CheckCond = document.getElementById("Chk");                              
 	if (CheckCond.checked) {                         
-		document.getElementById("fText").style.display = "inline";                
+		document.getElementById("fText").style.display = "inline";                 
 }*/                                                                                               
 	if (document.getElementById("Chk").checked) {                      
 		document.getElementById("fText").style.display = "inline";           
