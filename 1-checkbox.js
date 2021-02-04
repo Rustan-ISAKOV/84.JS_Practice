a900c1452f11e15cd3273f29e77efbf937844b3d
Rustan-ISAKOV <45059671+Rustan-ISAKOV@users.noreply.github.com>
2021-02-04 12:50:22 +0600

    Update 1-checkbox.js

--- 1-checkbox.js
+++ 1-checkbox.js
@@ -3,7 +3,7 @@
 	var CheckCond = document.getElementById("Chk");                                 
 	if (CheckCond.checked) {                           
 		document.getElementById("fText").style.display = "inline";                    
-}*/                                                                                                   
+}*/                                                                                                    
 	if (document.getElementById("Chk").checked) {                               
 	} else {            
 		document.getElementById("fText").style.display = "none";    
@@ -31,7 +31,7 @@ function onClickMe2() {
          
 function radioFun() {       
 	var radioArr = document.getElementsByName("r1");  
-	for (var i=0; i<radioArr.length; i++) { 
+	for (var i=0; i<radioArr.length; i++) {  
 		if (radioArr[i].checked) {
 			document.getElementById("fText").value = radioArr[i].value;
 		}
