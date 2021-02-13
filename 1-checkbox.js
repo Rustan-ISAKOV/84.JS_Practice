48c20aae923e90b21fa8931c29c2c571a3359c58
Rustan-ISAKOV <45059671+Rustan-ISAKOV@users.noreply.github.com>
2021-02-13 12:35:40 +0600

    Update 1-checkbox.js

--- 1-checkbox.js
+++ 1-checkbox.js
@@ -7,7 +7,7 @@
 	if (document.getElementById("Chk").checked) {                                     
 	} else {             
 		document.getElementById("fText").style.display = "none";      
-		document.getElementById("fText").value = "";      
+		document.getElementById("fText").value = "";       
 		document.getElementById("parag").style.display = "none";  
 	}  
 	document.getElementById("fText2").value = document.getElementById("fText").value;
